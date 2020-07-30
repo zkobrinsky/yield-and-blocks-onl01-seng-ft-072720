85e13e15d61658faf60a7018e56e75fc4aa56953
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-30 18:11:08 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -10,8 +10,8 @@ end
 
 # call your method here!
 
-# ["tim", "Tom", "Jim"].each do |name|
-#   if name.start_with?("T")
-#     puts "Hi, #{name}"
-#   end
-# end
+hello_t(["tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
