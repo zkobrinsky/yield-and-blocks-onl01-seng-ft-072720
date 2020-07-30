c2c66c0a972a7f4d8896f851ce4c0aa48e0e7eb4
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-30 18:17:03 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 def hello_t(array)
+  if block_given?
   i = 0
 
   while i < array.length
@@ -6,6 +7,8 @@ def hello_t(array)
     i = i+1
   end
     array
+  else
+    puts "Hey! No block was given!"
 end
 
 # call your method here!
